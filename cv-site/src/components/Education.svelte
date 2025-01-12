b404a3290213a037ce83ad6c0d92427b8bba671c
Novis616 <rlkost31@gmail.com>
2025-01-12 22:14:44 +0300

    deploy

---
+++ cv-site/src/components/Education.svelte
@@ -0,0 +1,33 @@
+<script>
+  import "../app.css";
+
+  export let education = [
+    {
+      institution: "ННГАСУ",
+      degree: "Бакалавр, Программная инженерия",
+      period: "2021 - 2024"
+    },
+    {
+      institution: "ПГТУ",
+      degree: "Бакалавр, Программная инженерия",
+      period: "2024 - 2026"
+    }
+  ];
+</script>
+
+<section id="education" class="py-16 bg-white shadow-lg rounded-lg max-w-4xl mx-auto mt-10">
+  <h2 class="font-heading text-4xl text-primary mb-4 text-center">Образование</h2>
+
+  {#each education as edu}
+    <div class="mb-6">
+      <h3 class="text-xl font-semibold text-gray-800 text-center">{edu.institution}</h3>
+      <p class="text-lg text-black-600 text-center"><strong>{edu.degree}</strong> ({edu.period})</p>
+    </div>
+  {/each}
+</section>
+
+<style>
+  section {
+    margin-bottom: 1.5rem;
+  }
+</style>
---
+++ cv-site/src/components/Skills.svelte
@@ -0,0 +1,24 @@
+<script>
+  import "../app.css";
+
+  export let skills = ["JavaScript", "Svelte", "HTML", "CSS", "Git", "Node.js"];
+</script>
+
+<section id="skills" class="py-16 bg-white shadow-lg rounded-lg max-w-4xl mx-auto mt-10">
+  <h2 class="font-heading text-4xl text-primary mb-4 text-center">Навыки</h2>
+
+  <ul class="list-disc pl-6 space-y-3">
+    {#each skills as skill}
+      <li class="text-lg leading-relaxed text-center">{skill}</li>
+    {/each}
+  </ul>
+</section>
+
+<style>
+  section {
+    margin-bottom: 1.5rem;
+  }
+  li {
+    list-style-type: none; /* Убираем маркеры */
+   }
+</style>
